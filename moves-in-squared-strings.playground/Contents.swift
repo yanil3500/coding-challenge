167c60479bec2ed8e7da944d9bf61294ea1f76d7
yanil3500 <yanil3500@gmail.com>
2017-03-28 09:03:06 -0700

    Currently solving movied-in-squared-strings

---
+++ moves-in-squared-strings.playground/Contents.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+//Moves in squared strings (II)
+
+//You are given a string of n lines, each substring being n characters long: For example:
+//
+//s = "abcd\nefgh\nijkl\nmnop"
+//
+//We will study some transformations of this square of strings.
+//
+/**
+ Clock rotation 180 degrees: rot
+rot(s) => "ponm\nlkji\nhgfe\ndcba"
+selfie_and_rot(s) (or selfieAndRot or selfie-and-rot) It is initial string + string obtained by clock rotation 180 degrees with dots interspersed in order (hopefully) to better show the rotation when printed.**/
+//s = "abcd\nefgh\nijkl\nmnop" -->
+//"abcd....\nefgh....\nijkl....\nmnop....\n....ponm\n....lkji\n....hgfe\n....dcba"
+
+
